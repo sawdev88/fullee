2fd47b4ac65d254e687c4059f7d7ba67ccdbeeca
Sean Wines <sean.wines@vuria.com>
2017-06-10 14:32:28 -0700

    Created landing video section with video logic and edit-toolbar

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,33 @@
 $(function () {
+  var videoCount = videos.length;
+  var randomVideo = Math.floor(Math.random() * videoCount);
 
-  // Info panel
-  $('.info').on('click', function () {
-    $('.info-panel').toggleClass('auto-size');
-    $('.info-container').fadeToggle('fast');
-    $(this).toggleClass('fa-dot-circle-o fa-close');
+  // Landing Video //
+  // Show Edit Toolbar
+  $('.open-toolbar').on('click', function () {
+    $('.edit-toolbar').css('top', 0);
+    $(this).fadeOut('fast');
   })
+
+  // Hide Toolbar
+  $('.close-toolbar').on('click', function () {
+    $('.edit-toolbar').css('top', '-6rem');
+    $('.open-toolbar').fadeIn('fast');
+  })
+
+  $('.hide-logo').on('click', function () {
+    $(this).toggleClass('selected');
+    $('.landing-logo').toggleClass('hide');
+  })
+
+  $('.random-icon').on('click', function () {
+    randomVideo = Math.floor(Math.random() * videoCount);
+    $('.landing-video video').attr('src', 'assets/video/' + videos[randomVideo].src)
+    $('.landing-title').text(videos[randomVideo].title)
+  })
+
+  // Display random video onload
+  $('.landing-video video').attr('src', 'assets/video/' + videos[randomVideo].src)
+  $('.landing-title').text(videos[randomVideo].title)
 
 })
---
+++ assets/scripts/videoList.js
@@ -0,0 +1,14 @@
+var videos = [
+  {
+    title: 'Test Title',
+    src: 'test.mp4'
+  },
+  {
+    title: 'Test Title 2',
+    src: 'test1.mp4'
+  },
+  {
+    title: 'Test Title 3',
+    src: 'test2.mp4'
+  }
+]
